1003804df5729b6d3cb1a2319dfe6d680ec958b2
Dominic Brugnano <dbgood@gmail.com>
2015-01-20 14:57:07 -0500

    multiplied the numbers and multiplied them to push out

--- exercise2.rb
+++ exercise2.rb
@@ -2,5 +2,8 @@
 puts 55 * 0.15
 # adding a string and an integer. Convert integer so that it works
 puts "The tip is " + 8.25.to_s
+#output results of 45628 multiplied by 7839 in a sentence using string interpolation
+number = 45628 * 7839
+puts "The value is #{number}"
 
 
